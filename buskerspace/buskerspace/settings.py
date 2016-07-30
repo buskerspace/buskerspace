f8b8bddf0b471e5dc56ea86c1ba22fc4af1c1df7
Eleanor <elmcmurtry1@gmail.com>
2016-07-30 14:14:11 +1000

    fixed INSTALLED_APPS

--- buskerspace/buskerspace/settings.py
+++ buskerspace/buskerspace/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+	'main.apps.MainConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'buskerspace.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'template/')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
