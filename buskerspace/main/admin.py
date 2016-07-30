99286ba706440396ffa27e836e5457e70f3e4cf7
Eleanor <elmcmurtry1@gmail.com>
2016-07-30 15:54:24 +1000

    setup admin

--- buskerspace/main/admin.py
+++ buskerspace/main/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
-# Register your models here.
+from .models import User, Busker, Event, Following
+admin.site.register(User)
+admin.site.register(Busker)
+admin.site.register(Event)
+admin.site.register(Following)
