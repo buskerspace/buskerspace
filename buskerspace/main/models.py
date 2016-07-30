3596c9e0dc122266a3fbd742611a1dd3b3a85555
Eleanor <elmcmurtry1@gmail.com>
2016-07-30 15:41:02 +1000

    fixed models.py

--- buskerspace/main/models.py
+++ buskerspace/main/models.py
@@ -1,11 +1,15 @@
 from __future__ import unicode_literals
-<<<<<<< HEAD
 from django.db import models
 
 
 class User(models.Model):
     email = models.CharField(max_length=100)
 
+	
+class Busker(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    busker_desc = models.CharField(max_length=3000)
+    genre = models.CharField(max_length=200)
 
 class Event(models.Model):
     event_datetime = models.DateTimeField()
@@ -15,22 +19,7 @@ class Event(models.Model):
     event_lat = models.DecimalField(max_digits=8, decimal_places=5)
     event_lng = models.DecimalField(max_digits=8, decimal_places=5)
 
-
-class Busker(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    busker_desc = models.CharField(max_length=3000)
-    genre = models.CharField(max_length=200)
-
-
-
 class Following(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     busker = models.ForeignKey(Busker, on_delete=models.CASCADE)
     email = models.CharField(max_length=100)
-    following =
-
-
-=======
-
-from django.db import models
->>>>>>> origin/master
