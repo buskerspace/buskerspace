da14e3ed3e7af3497f86611d466d6d32f16e6cac
Martin Middelmann <geek@mmarss.net>
2016-07-30 15:16:10 +1000

    Added first icon to map.js

--- buskerspace/webapp/map.js
+++ buskerspace/webapp/map.js
@@ -1,7 +1,46 @@
 function initMap() {
+
     var mapDiv = document.getElementById('map');
+
     var map = new google.maps.Map(mapDiv, {
-        center: {lat: 37.8136, lng: 144.9631},
-        zoom: 8
+        center: {lat: -33.91722, lng: 151.23064},
+        zoom: 8,
+        styles: [
+        {
+            featureType: 'poi',
+            elementType: 'geometry',
+            stylers: [
+                { visibility: 'on' },
+                { hue: '#1000e0' }
+            ]
+        }
+        ]
     });
+
+    var iconBase = 'https://maps.google.com/mapfiles/kml/shapes/';
+
+    var icons = {
+        info: {
+            icon: iconBase + 'info-i_maps.png'
+        }
+    };
+
+    function addMarker(feature) {
+        var marker = new google.maps.Marker({
+            position: feature.position,
+            icon: icons[feature.type].icon,
+            map: map
+        });
+    }
+
+    var features = [
+        {
+            position: new google.maps.LatLng(-33.91721, 151.22630),
+            type: 'info'
+        }
+    ];
+
+    for (var i = 0, feature; feature = features[i]; i++) {
+        addMarker(feature);
+    }
 }
