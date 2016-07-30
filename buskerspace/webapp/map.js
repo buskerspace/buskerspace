1e3ded072ca6c3763802e67fc16a8d1842c77c1f
Martin Middelmann <geek@mmarss.net>
2016-07-30 17:55:07 +1000

    Some of the filter core implemented.

--- buskerspace/webapp/map.js
+++ buskerspace/webapp/map.js
@@ -1,11 +1,35 @@
+var map = undefined;
+var events = undefined;
+var filters = undefined;
+
+/* Initialise the map */
 function initMap() {
 
     var initialCoords = {lat: -37.800089, lng: 144.964451};
 
-    var map = new google.maps.Map(document.getElementById('map'), {
+    map = new google.maps.Map(document.getElementById('map'), {
         zoom: 16,
         center: initialCoords
     });
+
+    events = {
+        musicalEvents: [
+            { }
+        ],
+        performanceEvents: [
+            { }
+        ],
+        otherEvents: [
+            { }
+        ],
+    };
+
+    filters = {
+        distance: 20,
+        musicalEvents: true,
+        performanceEvents: true,
+        otherEvents: true
+    };
 
     var event = {
         lat: -37.800089,
@@ -15,16 +39,19 @@ function initMap() {
         desc: 'The big event that\'s on today. There are lots of people there.'
     };
 
-    addEvent(map, event);
-
-    return map;
+    addEvent(event);
 }
 
-function addEvent(map, event) {
+/* Add the event event to the map */
+function addEvent(event) {
+
+    // Parse event
 
     var images = {
-        undef:   'https://maps.google.com/mapfiles/kml/shapes/info-i_maps.png',
-        musical: 'https://maps.google.com/mapfiles/kml/shapes/info-i_maps.png'
+        undef:       'https://maps.google.com/mapfiles/kml/shapes/info-i_maps.png',
+        musical:     'https://maps.google.com/mapfiles/kml/shapes/info-i_maps.png',
+        performance: 'https://maps.google.com/mapfiles/kml/shapes/info-i_maps.png',
+        other:       'https://maps.google.com/mapfiles/kml/shapes/info-i_maps.png'
     };
 
     var icon = images.undef;
@@ -33,7 +60,15 @@ function addEvent(map, event) {
         case 'musical':
             icon = images.musical;
             break;
+        case 'performance':
+            icon = images.performance;
+            break;
+        case 'other':
+            icon = images.other;
+            break;
     }
+
+    // Add marker
 
     var marker = new google.maps.Marker({
         map: map,
@@ -42,6 +77,7 @@ function addEvent(map, event) {
         icon: icon
     });
 
+    // Create InfoWindow
 
     var windowDiv   = document.createElement("div");
     var windowTitle = document.createElement("p");
@@ -62,6 +98,8 @@ function addEvent(map, event) {
         content: windowDiv
     });
 
+    // Add callbacks
+
     marker.addListener('click', function() {
         infoWindow.open(map, marker);
     });
@@ -70,4 +108,65 @@ function addEvent(map, event) {
         infoWindow.close();
     });
 
+    // Add event to list
+
+    var eventObj = {
+        marker:     marker,
+        infoWindow: infoWindow,
+        type:       event.type,
+        lat:        event.lat,
+        lng:        event.lng,
+        title:      event.title,
+        desc:       event.desc
+    }
+
+    switch (event.type) {
+        case 'musical':
+            events.musicalEvents.push(eventObj);
+            break;
+        case 'performance':
+            events.performanceEvents.push(eventObj);
+            break;
+        case 'other':
+            events.otherEvents.push(eventObj);
+            break;
+    }
+
+}
+
+/* Filter events */
+function filterEvents() {
+
+    var toggle;
+
+    // Musical
+    toggle = document.getElementById("checkbox-1").value;
+    if (toggle != filters.musicalEvents) {
+        setVisibility(events.musicalEvents, toggle);
+        filters.musicalEvents = toggle;
+    }
+
+    // Performance
+    toggle = document.getElementById("checkbox-2").value;
+    if (toggle != filters.performanceEvents) {
+        setVisibility(events.performanceEvents, toggle);
+        filters.musicalEvents = toggle;
+    }
+
+    // Other
+    toggle = document.getElementById("checkbox-3").value;
+    if (toggle != filters.otherEvents) {
+        setVisibility(events.otherEvents, toggle);
+        filters.musicalEvents = toggle;
+    }
+
+    function setVisibility(eventList, toggle) {
+
+        for (var i = 0, event; event = eventList[i]; i++) {
+            if (toggle)
+                event.marker.setMap(map);
+            else
+                event.marker.setMap(null);
+        }
+    }
 }
