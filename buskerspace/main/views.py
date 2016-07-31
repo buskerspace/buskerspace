75ac7c361fa259899868bef64a1fdd2a6b91cd27
Eleanor <elmcmurtry1@gmail.com>
2016-07-31 20:51:28 +1000

    changed search feature to be more user friendly (queries busker names, descriptions by keywords)

--- buskerspace/main/views.py
+++ buskerspace/main/views.py
@@ -9,6 +9,45 @@ from django.core.exceptions import ObjectDoesNotExist
 import datetime
 import time
 
+# Below snippet liberated from http://julienphalip.com/post/2825034077/adding-search-to-a-django-site-in-a-snap
+import re
+
+from django.db.models import Q
+
+def normalize_query(query_string,
+                    findterms=re.compile(r'"([^"]+)"|(\S+)').findall,
+                    normspace=re.compile(r'\s{2,}').sub):
+    ''' Splits the query string in invidual keywords, getting rid of unecessary spaces
+        and grouping quoted words together.
+        Example:
+        
+        >>> normalize_query('  some random  words "with   quotes  " and   spaces')
+        ['some', 'random', 'words', 'with quotes', 'and', 'spaces']
+    
+    '''
+    return [normspace(' ', (t[0] or t[1]).strip()) for t in findterms(query_string)] 
+
+def get_query(query_string, search_fields):
+    ''' Returns a query, that is a combination of Q objects. That combination
+        aims to search keywords within a model by testing the given search fields.
+    
+    '''
+    query = None # Query to search for every search term        
+    terms = normalize_query(query_string)
+    for term in terms:
+        or_query = None # Query to search for a given term in each field
+        for field_name in search_fields:
+            q = Q(**{"%s__icontains" % field_name: term})
+            if or_query is None:
+                or_query = q
+            else:
+                or_query = or_query | q
+        if query is None:
+            query = or_query
+        else:
+            query = query & or_query
+    return query
+	
 def search(request):
 	return render(request, "search.html")
 	
@@ -161,7 +200,7 @@ def createBusker(request):
 	return render(request, 'newbusker.html', { 'error_message': 'Success!' })
 
 def results(request):
-    buskers = Busker.objects.filter(busker_name__icontains=request.POST.get('search'))
+    buskers = Busker.objects.filter(get_query(request.POST.get('search'), [ 'busker_name', 'busker_desc', ]))
     if not buskers:
         return render(request, 'search.html', { 'error_message': 'No matches found. :(' })
     return render(request, 'searchresults.html', { 'buskers': buskers })
